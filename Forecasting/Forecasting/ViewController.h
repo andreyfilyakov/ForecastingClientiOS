be06e401d4404413ac148d69540b179d13d6110e
Andrey Filyakov <andreyfilyakov@gmail.com>
2015-11-16 23:35:27 +0300

    Initial project with cocoapods.

---
+++ Forecasting/Forecasting/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Forecasting
+//
+//  Created by Andrey Filyakov on 16.11.15.
+//  Copyright © 2015 Andrey Filyakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Forecasting/Forecasting/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Forecasting
+//
+//  Created by Andrey Filyakov on 16.11.15.
+//  Copyright © 2015 Andrey Filyakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
